227acb746ceacc0b8528ef0c592b79709aadd85a
DanSML <danlagoa739@gmail.com>
2022-08-09 00:20:36 -0300

    feat: add import category use case and controller

---
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -0,0 +1,55 @@
+import fs from "fs"
+import { parse } from "csv-parse"
+import { ICategoriesRepository } from "../../repositories/ICategoriesRepository"
+import { inject, injectable } from "tsyringe"
+
+interface IImportCategory {
+  name: string
+  description: string
+}
+
+@injectable()
+export class ImportCategoryUseCase { 
+  constructor (@inject("CategoriesRepository") private categoriesRepository: ICategoriesRepository) {}
+  
+  loadCategories(file: Express.Multer.File): Promise<IImportCategory[]> {
+    return new Promise((resolve, reject) => {
+      const stream = fs.createReadStream(file.path)
+      const categories: IImportCategory[] = []
+  
+      const parseFile = parse()
+  
+      stream.pipe(parseFile)
+  
+      parseFile.on("data", async (line) => {
+        const [name, description] = line
+        categories.push({
+          name, 
+          description
+        })
+      }).on("end", () => {
+        fs.promises.unlink(file.path)
+        resolve(categories)
+      }).on("error", (error) => {
+        reject(error)
+      })
+    })
+  }
+
+  async execute(file: Express.Multer.File): Promise<void> {
+    const categories = await this.loadCategories(file)
+    
+    categories.map(async (category) => {
+      const {name, description} = category
+      const existCategory = await this.categoriesRepository.findByName(name)
+
+      if (existCategory) {
+        throw new Error("Category already exists!")
+      }
+
+      if (!existCategory) {
+        await this.categoriesRepository.create({name, description})
+      }
+    })
+  }
+}
