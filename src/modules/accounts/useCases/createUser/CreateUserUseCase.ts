a14b8bc6c339ebfae31c9661a80ecc2950cfae27
DanSML <danlagoa739@gmail.com>
2022-08-10 19:16:59 -0300

    feat: add create user use case and controller

---
+++ src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
@@ -0,0 +1,17 @@
+import { inject, injectable } from "tsyringe";
+import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
+import { IUsersRepository } from "../../repositories/IUserRepository";
+
+@injectable()
+export class CreateUserUseCase {
+  constructor(
+    @inject("UsersRepository")
+    private usersRepository: IUsersRepository
+  ) {}
+
+  async execute({name, username, email, password, driver_license}: ICreateUserDTO): Promise<void> {
+    await this.usersRepository.create({
+      name, username, email, password, driver_license
+    })
+  }
+}
